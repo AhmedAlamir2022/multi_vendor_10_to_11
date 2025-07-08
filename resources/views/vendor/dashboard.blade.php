78cadf982fd98592be6508f1c4cd884df5e452c2
Ahmed Alamir <ahmedalamir521@gmail.com>
2025-07-08 10:01:42 +0300

    vendor_dashboard

--- resources/views/vendor/dashboard.blade.php
+++ resources/views/vendor/dashboard.blade.php
@@ -1,13 +1,108 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Vendor Dashboard</title>
-</head>
-<body>
-    <h1>Vendor Dashboard</h1>
+@extends('vendor.layouts.master')
+@section('title')
+    Vendor Dashboard || Multi Vendor
+@endsection
+@section('content')
+    <section id="wsus__dashboard">
+        <div class="container-fluid">
+            @include('vendor.layouts.sidebar')
+            <div class="row">
+                <div class="col-xl-9 col-xxl-10 col-lg-9 ms-auto">
+                    <div class="dashboard_content">
+                        <div class="wsus__dashboard">
+                            <div class="row">
+                                <div class="col-xl-2 col-6 col-md-4">
+                                    <a class="wsus__dashboard_item red" href="">
+                                        <i class="fas fa-cart-plus"></i>
+                                        <p>Today's Orders</p>
+                                        <h4 style="color:#ffff">44</h4>
+                                    </a>
+                                </div>
+                                <div class="col-xl-2 col-6 col-md-4">
+                                    <a class="wsus__dashboard_item red" href="">
+                                        <i class="fas fa-cart-plus"></i>
+                                        <p>Td's Pending Orders</p>
+                                        <h4 style="color:#ffff">44</h4>
+                                    </a>
+                                </div>
+                                <div class="col-xl-2 col-6 col-md-4">
+                                    <a class="wsus__dashboard_item red" href="">
+                                        <i class="fas fa-cart-plus"></i>
+                                        <p>Total Orders</p>
+                                        <h4 style="color:#ffff">44</h4>
+                                    </a>
+                                </div>
+                                <div class="col-xl-2 col-6 col-md-4">
+                                    <a class="wsus__dashboard_item red" href="">
+                                        <i class="fas fa-cart-plus"></i>
+                                        <p>Total Pending Orders</p>
+                                        <h4 style="color:#ffff">44</h4>
+                                    </a>
+                                </div>
+                                <div class="col-xl-2 col-6 col-md-4">
+                                    <a class="wsus__dashboard_item red" href="">
+                                        <i class="fas fa-cart-plus"></i>
+                                        <p>Completed Orders</p>
+                                        <h4 style="color:#ffff">44</h4>
+                                    </a>
+                                </div>
+                                <div class="col-xl-2 col-6 col-md-4">
+                                    <a class="wsus__dashboard_item red" href="">
+                                        <i class="fas fa-cart-plus"></i>
+                                        <p>Total Products</p>
+                                        <h4 style="color:#ffff">44</h4>
+                                    </a>
+                                </div>
+                                <div class="col-xl-2 col-6 col-md-4">
+                                    <a class="wsus__dashboard_item red" href="javascript:;">
+                                        <i class="fas fa-cart-plus"></i>
+                                        <p>Todays Earnings</p>
+                                        <h4 style="color:#ffff">44</h4>
+                                    </a>
+                                </div>
+                                <div class="col-xl-2 col-6 col-md-4">
+                                    <a class="wsus__dashboard_item red" href="javascript:;">
+                                        <i class="fas fa-cart-plus"></i>
+                                        <p>This Months Earnings</p>
+                                        <h4 style="color:#ffff">44</h4>
+                                    </a>
+                                </div>
+                                <div class="col-xl-2 col-6 col-md-4">
+                                    <a class="wsus__dashboard_item red" href="javascript:;">
+                                        <i class="fas fa-cart-plus"></i>
+                                        <p>This Year Earnings</p>
+                                        <h4 style="color:#ffff">44</h4>
+                                    </a>
+                                </div>
 
-</body>
-</html>
+                                <div class="col-xl-2 col-6 col-md-4">
+                                    <a class="wsus__dashboard_item red" href="javascript:;">
+                                        <i class="fas fa-cart-plus"></i>
+                                        <p>Total Earnings</p>
+                                        <h4 style="color:#ffff">44</h4>
+                                    </a>
+                                </div>
+                                <div class="col-xl-2 col-6 col-md-4">
+                                    <a class="wsus__dashboard_item red" href="">
+                                        <i class="fas fa-cart-plus"></i>
+                                        <p>Total Reviews</p>
+                                        <h4 style="color:#ffff">44</h4>
+                                    </a>
+                                </div>
+                                <div class="col-xl-2 col-6 col-md-4">
+                                    <a class="wsus__dashboard_item red" href="">
+                                        <i class="fas fa-user-shield"></i>
+                                        <p>shop profile</p>
+                                        <h4 style="color:#ffff">44</h4>
+                                    </a>
+                                </div>
+
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+@endsection
